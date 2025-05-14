c566aceb194d1e93ab08d30e1b0bbddff5cb14eb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-05-14 19:58:39 +0530

    learn memory

--- src/lib/practice/official/memory.ts
+++ src/lib/practice/official/memory.ts
@@ -2,10 +2,24 @@ import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
 import { openai } from "@ai-sdk/openai";
 import { gemini } from "../../models";
+import { LibSQLStore, LibSQLVector } from "@mastra/libsql";
 
 export const myMemoryAgent = new Agent({
   name: "Memory Agent",
   instructions: "You are a helpful assistant.",
   model: gemini,
-  memory: new Memory({}),
+  memory: new Memory({
+    storage: new LibSQLStore({ url: "file:../../../../memory.db" }),
+    options: {
+      lastMessages: 100,
+      semanticRecall: false,
+      threads: {
+        generateTitle: true,
+      },
+      workingMemory: {
+        enabled: true,
+        use: "tool-call",
+      },
+    },
+  }),
 });
